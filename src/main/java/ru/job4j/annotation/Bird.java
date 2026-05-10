17e668c6cadcfc33d41fccd1c594fe4cbe6a6f09
Dmitrii <dmitrijdobrydin@gmail.com>
2026-05-10 16:38:31 +0300

    DI

---
+++ src/main/java/ru/job4j/annotation/Bird.java
@@ -0,0 +1,15 @@
+package ru.job4j.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bird implements Pet<Bird> {
+    @Value("${bird.name:simpleBird}")
+    private String name = "Bird";
+
+    @Override
+    public String print() {
+        return "Bird name = %s".formatted(name);
+    }
+}
